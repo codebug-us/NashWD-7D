3e5cd75fa2f34f51c9a76881e8e6b66c8c5f2e5a
aemiller2013 <alexandra.e.miller@vanderbilt.edu>
2019-05-14 14:04:46 -0500

    completed task

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
 
+$("#send-button").click(function(){
+	console.log("the button was clicked");
+	var ourMessage=$("#message-box").val();
+	console.log(ourMessage);
 
+
+	var request = $.ajax({
+ 		url: "https://hooks.slack.com/services/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",
+ 		method: "POST",
+ 		data: JSON.stringify({text:ourMessage}),
+ 	});
+ 	request.done(function(response){
+ 		console.log("Slack response: ", response);
+ 	});
+
+
+});
 
 
 
